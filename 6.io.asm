4fe8ed46529948d74effe6485c9f3bd778188a1f
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-09-28 12:08:44 +0200

    Add files via upload

---
+++ 6.io.asm
@@ -0,0 +1,21 @@
+	.data
+	
+msg:    .asciiz "Some message\n"
+num:	.word   1111
+
+	.text
+	lw $a0, num
+	li $v0, 1
+	syscall
+	
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+	
+	la $a0, msg
+	li $v0, 4
+	syscall
+	
+	li $a0, '.'
+	li $v0, 11
+	syscall
