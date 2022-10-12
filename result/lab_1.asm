8c7a91f6162cb83a94ae1ddf5d0a348073874e7a
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-10-12 13:47:52 +0200

    Update and rename lab_1 to lab_1.asm

---
+++ result/lab_1.asm
@@ -0,0 +1,26 @@
+	  .data
+A:	.float -3
+B:	.float 19
+C:	.float 1
+D:	.float 0.2
+E:	.float 4
+
+	  .text
+	  .globl main             # (A-B)/(C+D)-E
+  
+main:
+	  l.s   $f0,  A
+	  l.s   $f1,  B
+	  sub.s $f2,  $f0, $f1
+	  l.s   $f0,  C
+	  l.s   $f1,  D
+	  add.s $f3,  $f0, $f1
+	  div.s $f0,  $f2, $f3
+	  l.s   $f1,  E
+	  sub.s $f12, $f0, $f1    
+	
+	  li    $v0,  2           # print float result
+	  syscall
+	
+	  li    $v0,  10          # exit program
+	  syscall
