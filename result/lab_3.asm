1539919385c263d8c102f8e80e1ba638548be05f
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-10-16 02:46:55 +0200

    new version of lab_3

--- result/lab_3.asm
+++ result/lab_3.asm
@@ -1,238 +1,164 @@
 	.data
-	
-x_1:	.byte 0, 0, 0, 1, 0, 0, 1
-x_2:	.byte 1, 0, 1, 1, 1, 0, 0
-x_3:	.byte 1, 1, 1, 0, 1, 1, 1
-x_4:	.byte 1, 1, 1, 1, 1, 1, 0
-size:	.byte 7
+
+prmpt1:	.asciiz "\nType 7 zeros or units:\n"
+prmpt2: .asciiz "\nDo you want to check sequence again?\nType 'y' to continue or something else to exit.\n"
+buffer:	.space  2
+err_in: .asciiz "Wrong number. Input should contain only 1 or 0.\n"
+input:  .byte 	0, 0, 0, 0, 0, 0, 0
+mask1:	.byte 	0, 0, 0, 1, 0, 0, 1
+mask2:	.byte 	1, 0, 1, 1, 1, 0, 0
+mask3:	.byte 	1, 1, 1, 0, 1, 1, 1
+mask4:	.byte 	1, 1, 1, 1, 1, 1, 0
+size:	.byte 	7
+y1:	.asciiz "y1: "
+y2:	.asciiz "y2: "
+y3:	.asciiz "y3: "
 
 	.text
 	.globl main
 main:
-	la   $s1, x_1		# address of the first memory sell
-	lb   $t0, size		# array's size
-	li   $t1, 0		# counter for array's cells
-	
-	li   $t4, 1		# future result of x1 & x2 & ... &x7 = Fn
-	li   $t5, 0		# future resulf of F1 || F2 || ... || Fn
-y1.1:	
-	beq  $t1, $t0, end_y1.1
-	lb   $t3, ($s1)
-	beq  $t1, 3, continue_y1.1
-	beq  $t1, 6, continue_y1.1
-	
-	jal  b_inv
-	
-continue_y1.1:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y1.1
-	
-end_y1.1:
-	
-	la   $s1, x_2
+	lb   $s0, size
+loop:	
+	la   $s1, input
 	li   $t1, 0
-	or   $t5, $t5, $t4
-	li   $t4, 1
-y1.2:	
-	beq  $t1, $t0, end_y1.2
-	lb   $t3, ($s1)
-	beq  $t1, 0, continue_y1.2
-	beq  $t1, 2, continue_y1.2
-	beq  $t1, 3, continue_y1.2
-	beq  $t1, 4, continue_y1.2
-	
-	jal  b_inv
-	
-continue_y1.2:
 
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y1.2
-	
-end_y1.2:
-
-	or $t5, $t5, $t4
-	
-	la   $s1, x_3
-	li   $t1, 0
-	li   $t4, 1
-
-y1.3:	
-	beq  $t1, $t0, end_y1.3
-	lb   $t3, ($s1)
-	bne  $t1, 3, continue_y1.3
-	
-	jal  b_inv
-	
-continue_y1.3:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y1.3
-	
-end_y1.3:
-
-	or $t5, $t5, $t4
-	
-	la   $s1, x_4
-	li   $t1, 0
-	li   $t4, 1
-
-y1.4:	
-	beq  $t1, $t0, end_y1.4
-	lb   $t3, ($s1)
-	bne  $t1, 6, continue_y1.4
-	
-	jal  b_inv
-	
-continue_y1.4:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y1.4
-	
-end_y1.4:
-
-	or   $t5, $t5, $t4
-
-	move $a0, $t5
-	li   $v0, 1
+	la   $a0, prmpt1
+	li   $v0, 4
 	syscall
 	
-	li   $a0, '\n'
+	read:
+	beq  $s0, $t1, end_read
+	
+	li   $v0, 5
+	syscall
+	
+	blt  $v0, 0, err_wrong_input
+	bgt  $v0, 1, err_wrong_input
+	
+	sb   $v0, ($s1)
+	
+	la   $s1, 1($s1)
+	add  $t1, $t1, 1
+	
+	j    read
+	end_read:
+	
+	li   $t4, 0			# result of DNF
+	
+	la   $s2, mask1			# $s2 - register for mask's address
+	jal  DNF
+	beq  $t4, 1, y1_output
+	
+	la   $s2, mask2
+	jal  DNF
+	beq  $t4, 1, y1_output
+	
+	la   $s2, mask3
+	jal  DNF
+	beq  $t4, 1, y1_output
+	
+	la   $s2, mask4
+	jal  DNF
+	
+y1_output:
+
+	li   $a0, 10
 	li   $v0, 11
 	syscall
 	
-	la   $s1, x_2
-	li   $t1, 0
-	li   $t4, 1
-	li   $t5, 0
-
-y2.2:	
-	beq  $t1, $t0, end_y2.2
-	lb   $t3, ($s1)
-	beq  $t1, 0, continue_y2.2
-	beq  $t1, 2, continue_y2.2
-	beq  $t1, 3, continue_y2.2
-	beq  $t1, 4, continue_y2.2
+	la   $a0, y1
+	jal  y_out
 	
-	jal  b_inv
+	li   $t4, 0
 	
-continue_y2.2:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
+	la   $s2, mask2
+	jal  DNF
+	beq  $t4, 1, y2_output
 	
-	j y2.2
+	la   $s2, mask3
+	jal  DNF
 	
-end_y2.2:
-
-	or   $t5, $t5, $t4
-
-	la   $s1, x_3
-	li   $t1, 0
-	li   $t4, 1
+y2_output:
+	la   $a0, y2
+	jal  y_out
 	
-y2.3:	
-	beq  $t1, $t0, end_y2.3
-	lb   $t3, ($s1)
-	bne  $t1, 3, continue_y2.3
+	li   $t4, 0
 	
-	jal  b_inv
+	la   $s2, mask1
+	jal  DNF
+	beq  $t4, 1, y2_output
 	
-continue_y2.3:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
+	la   $s2, mask3
+	jal  DNF
 	
-	j y2.3
+y3_output:
+	la   $a0, y3
+	jal  y_out
 	
-end_y2.3:
-
-	or $t5, $t5, $t4
-	
-	move $a0, $t5
-	li   $v0, 1
+question:	
+	la   $a0, prmpt2
+	li   $v0, 4
 	syscall
 	
-	li   $a0, '\n'
-	li   $v0, 11
+	la   $a0, buffer
+	li   $a1, 2
+	li   $v0, 8
 	syscall
 	
-	la   $s1, x_1
-	li   $t1, 0
-	li   $t4, 1
-	li   $t5, 0
-
-y3.1:	
-	beq  $t1, $t0, end_y3.1
-	lb   $t3, ($s1)
-	beq  $t1, 3, continue_y3.1
-	beq  $t1, 6, continue_y3.1
-	
-	jal  b_inv
-	
-continue_y3.1:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y3.1
-	
-end_y3.1:
-
-	or   $t5, $t5, $t4
-
-	la   $s1, x_3
-	li   $t1, 0
-	li   $t4, 1
-	
-y3.3:	
-	beq  $t1, $t0, end_y3.3
-	lb   $t3, ($s1)
-	bne  $t1, 3, continue_y3.3
-	
-	jal  b_inv
-	
-continue_y3.3:
-
-	and  $t4, $t4, $t3
-	add  $t1, $t1, 1
-	la   $s1, 1($s1)
-	
-	j y3.3
-	
-end_y3.3:
-
-	or $t5, $t5, $t4
-	
-	move $a0, $t5
-	li   $v0, 1
-	syscall
+	lb   $a0, buffer
+	beq  $a0, 121, loop
 	
 	li   $v0, 10
 	syscall
-
-
 	
-b_inv:				# custom integer inversion: converts 0 to 1, any non-zero number to 0
-	beqz $t3, is_zero
-	li   $t3, 0
-	j    is_unit
-is_zero:
-	li   $t3, 1
-is_unit:
+err_wrong_input:
+
+	la   $a0, err_in
+	li   $v0, 4
+	syscall
+
+	j    question
+end_err_wrong_input:
+	
+y_out:
+	li   $v0, 4			# address of string should be already loaded
+	syscall
+	
+	move $a0, $t4
+	li   $v0, 1
+	syscall
+	
+	li   $a0, 10
+	li   $v0, 11
+	syscall
+	jr   $ra
+y_out_end:
+	
+DNF:					# disjunctive normal form
+	move $s3, $ra
+	li   $t0, 1			# result of comparation with mask
+	la   $s1, input
+	
+	jal  comparation_mask
+	
+	or   $t4, $t4, $t0
+	
+	jr   $s3
+end_DNF:
+
+comparation_mask:
+	li   $t1, 0			# counter
+	start_comparation_mask:
+	beq  $s0, $t1, end_comparation_mask
+	lb   $t2, ($s1)
+	lb   $t3, ($s2)
+	
+	add  $t1, $t1, 1
+	la   $s1, 1($s1)
+	la   $s2, 1($s2)
+	
+	beq  $t3, $t2, start_comparation_mask
+	li   $t0, 0  
+end_comparation_mask:
+	
 	jr   $ra
