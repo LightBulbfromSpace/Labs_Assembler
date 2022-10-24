5d732d3e82cfd45c80236c55e762de8bb4c5f6b5
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-10-24 10:14:14 +0200

    lab_1.asm: made program with double precision floating point

--- result/lab_1.asm
+++ result/lab_1.asm
@@ -1,25 +1,25 @@
 	.data
-A:	.float -3
-B:	.float 19
-C:	.float 1
-D:	.float 0.2
-E:	.float 4
+A:	.double -3
+B:	.double 19
+C:	.double 1
+D:	.double 0.2
+E:	.double 4
 
 	.text
 	.globl main             # (A-B)/(C+D)-E
   
 main:
-	l.s   $f0,  A
-	l.s   $f1,  B
-	sub.s $f2,  $f0, $f1
-	l.s   $f0,  C
-	l.s   $f1,  D
-	add.s $f3,  $f0, $f1
-	div.s $f0,  $f2, $f3
-	l.s   $f1,  E
-	sub.s $f12, $f0, $f1    
+	l.d   $f0,  A
+	l.d   $f2,  B
+	sub.d $f4,  $f0, $f2
+	l.d   $f0,  C
+	l.d   $f2,  D
+	add.d $f0,  $f0, $f2
+	div.d $f0,  $f4, $f0
+	l.d   $f2,  E
+	sub.d $f12, $f0, $f2    
 	
-	li    $v0,  2           # print float result
+	li    $v0,  3           # print double result
 	syscall
 	
 	li    $v0,  10          # exit program
