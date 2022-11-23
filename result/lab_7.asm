0f2b1881471f41cce11d9469a4ab73539205b7ef
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-11-23 14:37:50 +0200

    lab_7.asm: fixed all bugs, added wrong user input handler

--- result/lab_7.asm
+++ result/lab_7.asm
@@ -6,63 +6,83 @@
 .end_macro
 
 .macro print_str(%src_buf)
-	la   $a0, %src_buf
-	li   $v0, 4
+	la     $a0, %src_buf
+	li     $v0, 4
 	syscall
 .end_macro
 
-.macro print_double		# value to print should be already set in $f12
+.macro print_double		 # value to print should be already set in $f12
 	li  $v0, 3
 	syscall
 .end_macro
+
+   	.kdata
+msg:	.asciiz "Wrong input"
+
+	.ktext 0x80000180
+   	la   $a0, msg
+   	li   $v0, 4
+   	syscall
+   
+   	la   $k0, done
+   	mtc0 $k0, $14
+   	eret
+
 	.data
-prmpt:  .asciiz "Put in A, B, C values for Ax^2+Bx+C=\n"
+prmpt:  .asciiz "Enter A, B, C values for Ax^2+Bx+C\n"
+prt_A:  .asciiz "A: "
+prt_B:  .asciiz "B: "
+prt_C:  .asciiz "C: "
 prt_r1: .asciiz "\nFirst root:\n"
 prt_r2: .asciiz "\nSecond root:\n"
-n_prpt: .asciiz "No real roots."
+n_prpt: .asciiz "\nNo real roots."
+
 	.text
 	print_str(prmpt)
+	print_str(prt_A)
+	read_double($f2)	 # A
+	print_str(prt_B)
+	read_double($f4)	 # B
+	print_str(prt_C)
+	read_double($f6)	 # C
 	
-	read_double($f2)	# A
-	read_double($f4)	# B
-	read_double($f6)	# C
+	mul.d   $f8,  $f4,  $f4  # B^2
 	
-	mul.d  $f8,  $f4,  $f2	# B^2
+	mul.d   $f10, $f2,  $f6	 # A*C
+	li      $t0,  4
+	mtc1    $t0,  $f12
+	cvt.d.w $f12, $f12
+	mul.d   $f10, $f10, $f12 # 4*A*C
 	
-	#mov.d  $f12, $f8
-	#print_double
+	sub.d   $f8,  $f8,  $f10 # B^2 - 4*A*C
 	
-	mul.d  $f10, $f2,  $f6	# A*C
-	li     $t0,  4
-	mtc1   $t0,  $f12
-	mul.d  $f10, $f10, $f12 # 4*A*C
-	sub.d  $f8,  $f8,  $f10 # B^2 - 4*A*C
+	cvt.w.d $f14, $f8
+	mfc1.d  $t0,  $f14
 	
-	mfc1.d $t0,  $f8
-	bltz   $t0,  _neg
+	bltz    $t0,  _neg
 	
-	sqrt.d $f8,  $f8	# D^(1/2)
+	sqrt.d  $f8,  $f8	 # D^(1/2)
 	
-	mov.d  $f12, $f8
-	print_double
-	
-	li     $t0,  -1
-	mtc1   $t0,  $f10
-	mul.d  $f4,  $f4,  $f10	# -B
-	add.d  $f12, $f4,  $f8	# -B+D^(1/2)
-	li     $t0,  2
-	mtc1   $t0,  $f10
-	mul.d  $f2,  $f2,  $f10 # 2*A
-	div.d  $f12, $f12, $f2  # (-B+D^(1/2))/(2*A)
+	li      $t0,  -1
+	mtc1    $t0,  $f10
+	cvt.d.w $f10, $f10
+	mul.d   $f4,  $f4,  $f10 # -B
+	add.d   $f12, $f4,  $f8	 # -B+D^(1/2)
+	li      $t0,  2
+	mtc1    $t0,  $f10
+	cvt.d.w $f10, $f10
+	mul.d   $f2,  $f2,  $f10 # 2*A
+	div.d   $f12, $f12, $f2  # (-B+D^(1/2))/(2*A)
 	print_str(prt_r1)
 	print_double
-	sub.d  $f12, $f4,  $f8  # -B-D^(1/2)
-	div.d  $f12, $f12, $f2  # (-B-D^(1/2))/(2*A)
+	
+	sub.d   $f12, $f4,  $f8  # -B-D^(1/2)
+	div.d   $f12, $f12, $f2  # (-B-D^(1/2))/(2*A)
 	print_str(prt_r2)
 	print_double
 	j      done
 _neg:
 	print_str(n_prpt)
 done:
-	li $v0, 10
+	li      $v0, 10
 	syscall	
